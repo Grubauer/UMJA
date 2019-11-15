309b58f289ea355ac5d875000f86bba8cf70cac6
Grubauer <44618705+Grubauer@users.noreply.github.com>
2019-11-15 12:08:05 +0100

    Initial commit

---
+++ Logic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace UMJA
+{
+    public class Logic
+    {
+        public static void ReadDocument(string path)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("C:\Users\max\source\repos\UMJA\UMJA\uml.xml");
+
+            var x = doc.ChildNodes;
+
+        }
+    }
+}
