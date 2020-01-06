bd669c7138d4faa08e4c3a4e4c0f245c75b9fd9a
mgebhart16 <mgebhart56@gmail.com>
2020-01-06 20:17:50 +0100

    tests_commit

--- TestLibrary/UMJATest.cs
+++ TestLibrary/UMJATest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using Xunit;
 using Xunit.Abstractions;
 
 namespace TestLibrary {
+    
     public class UMJATest {
         private readonly ITestOutputHelper output;
 
@@ -85,6 +87,132 @@ namespace TestLibrary {
             Assert.Equal(expectedParameterType, methods[0].Parameters[0].ObjectType);
 
         }
+
+        [Fact]
+        public void TestMethod1() //Check if projectfolder exists  !!!Wichtig alle Aufrufe der Methode guiLog müssen beim Testen auskommentiert werden!!!
+        {
+            var javaObject = Logic.ReadDocument(@"Test1.graphml");
+            Logic.CreateProject(javaObject, @"C:\Users\mgebh\Desktop\", "Test1");
+
+            bool expRes = true;
+            bool res = false;
+            if (Directory.Exists(@"C:\Users\mgebh\Desktop\Test1")) res = true;
+
+            Assert.Equal(expRes, res);
+        }
+
+        [Fact]
+        public void TestMethod2() //Check folders1  
+        {
+            var javaObject = Logic.ReadDocument(@"Test1.graphml");
+            Logic.CreateProject(javaObject, @"C:\Users\mgebh\Desktop\", "Test1");
+
+
+            bool expRes = true;
+            bool res = false;
+            if (Directory.Exists(@"C:\Users\mgebh\Desktop\Test1\net")) res = true;
+
+            Assert.Equal(expRes, res);
+        }
+
+        [Fact]
+        public void TestMethod3() //Check folders2 
+        {
+            var javaObject = Logic.ReadDocument(@"Test1.graphml");
+            Logic.CreateProject(javaObject, @"C:\Users\mgebh\Desktop\", "Test1");
+
+
+            bool expRes = true;
+            bool res = false;
+            if (Directory.Exists(@"C:\Users\mgebh\Desktop\Test1\net\htlgrieskirchen")) res = true;
+
+            Assert.Equal(expRes, res);
+        }
+
+        [Fact]
+        public void TestMethod4() //Check folders3  
+        {
+            var javaObject = Logic.ReadDocument(@"Test1.graphml");
+            Logic.CreateProject(javaObject, @"C:\Users\mgebh\Desktop\", "Test1");
+
+
+            bool expRes = true;
+            bool res = false;
+            if (Directory.Exists(@"C:\Users\mgebh\Desktop\Test1\net\htlgrieskirchen\test1")) res = true;
+
+            Assert.Equal(expRes, res);
+        }
+
+        [Fact]
+        public void TestMethod5() //Check files  
+        {
+            var javaObject = Logic.ReadDocument(@"Test1.graphml");
+            Logic.CreateProject(javaObject, @"C:\Users\mgebh\Desktop\", "Test1");
+
+
+            bool expRes = true;
+            bool res = false;
+            if (File.Exists(@"C:\Users\mgebh\Desktop\Test1\net\htlgrieskirchen\test1\Student.java") &&
+                File.Exists(@"C:\Users\mgebh\Desktop\Test1\net\htlgrieskirchen\test1\School.java")) res = true;
+
+            Assert.Equal(expRes, res);
+        }
+
+        [Fact]
+        public void ParseVariables1() //Check variables  
+        {
+            var variables = UMJA.Utility.Variable.ParseVariables("+ city : String");
+
+
+            bool result = false;
+
+            foreach (var variable in variables)
+            {
+                if (variable.IsFinal == false && variable.IsPrivate == false &&
+                    variable.IsStatic == false && variable.Name == "city" && variable.ObjectType == "String"
+                    && variable.DefinedValue == null) result = true;
+            }
+
+            Assert.Equal(true, result);
+        }
+
+        [Fact]
+        public void ParseVariables2() //Check variables  
+        {
+            var variables = UMJA.Utility.Variable.ParseVariables("-  postcode : int");
+
+
+            bool result = false;
+
+            foreach (var variable in variables)
+            {
+                if (variable.IsFinal == false && variable.IsPrivate == true &&
+                    variable.IsStatic == false && variable.Name == "postcode" && variable.ObjectType == "int"
+                    && variable.DefinedValue == null) result = true;
+            }
+
+            Assert.Equal(true, result);
+        }
+
+
+        [Fact]
+        public void ParseVariables3() //Check variables  
+        {
+            var variables = UMJA.Utility.Variable.ParseVariables("-&lt; u & gt; Student: STUDENT");
+
+
+            bool result = false;
+
+            foreach (var variable in variables)
+            {
+                if (variable.IsFinal == false && variable.IsPrivate == true ||
+                    variable.IsStatic == true && variable.Name == "STUDENT" && variable.ObjectType == "Student"
+                    ) result = true;
+            }
+
+            Assert.Equal(true, result);
+        }
+
 
     }
 }
