fda2c1058d9500a5d98a1b912c442391cf05ef25
Grubauer <44618705+Grubauer@users.noreply.github.com>
2020-01-06 16:58:23 +0100

    Static fix: Wenn eine Methode oder Variable static ist sind nicht mehr automatisch alle Static

--- UMJA/Utility/Variable.cs
+++ UMJA/Utility/Variable.cs
@@ -15,38 +15,92 @@ namespace UMJA.Utility
         public string DefinedValue { get; set; }
         public static List<Variable> ParseVariables(string vString)
         {
-            bool staticV = vString.Contains("<u>");
 
-            
-            vString = Regex.Replace(vString, "<.*?>", string.Empty);
-            string[] variableStrings = vString.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-            List<Variable> variables = new List<Variable>();
-            foreach (var varString in variableStrings)
+
+            if (vString.Contains("<html>"))
             {
-               ;
-                if (varString.Trim().StartsWith("+") || varString.Trim().StartsWith("-") || varString.Trim().StartsWith("#"))
-                {
-                    string definedValue = null;
-                    string name;
-                    if (varString.Contains("="))
-                    {
-                        definedValue = varString.Split(':')[0].Split('=')[1].Trim();
-                        name = varString.Split(':')[0].Split('=')[0].Trim().Remove(0, 1).Trim();
-                    }
-                    else name = varString.Split(':')[0].Trim().Remove(0,1).Trim();
-                        
-                    bool isPrivate = varString.StartsWith("-");
-                   
-                    string objectType = varString.Split(':')[1].Trim();
-                    bool isFinal = name.ToUpper().Equals(name);
-                    
+                vString = vString.Replace("<html>", "");
+                vString = vString.Replace("</html>", "");
 
-                    variables.Add(new Variable { Name = name, IsPrivate = isPrivate, IsStatic = staticV, IsFinal = isFinal, ObjectType = objectType , DefinedValue = definedValue});
+                List<Variable> variables = new List<Variable>();
+                if (vString.Contains("<br>"))
+                {
+                    string[] variableStrings = vString.Split(new string[] { "<br>" }, StringSplitOptions.None);
+                    foreach (var variableString in variableStrings)
+                    {
+                        bool isStatic = variableString.Contains("<u>");
+                        string modVarString = variableString;
+                        if (isStatic)
+                        {
+                            modVarString = modVarString.Replace("<u>", "");
+                            modVarString = modVarString.Replace("</u>", "");
+                            variables.Add(ParseNormalVariable(modVarString.Trim(), true));
+                        }
+                        else
+                        {
+                            variables.Add(ParseNormalVariable(modVarString.Trim(), false));
+                        }
+                    }
+                }
+                else
+                {
+                    bool isStatic = vString.Contains("<u>");
+                    if (isStatic)
+                    {
+                        vString = vString.Replace("<u>", "");
+                        vString = vString.Replace("</u>", "");
+                        variables.Add(ParseNormalVariable(vString.Trim(), true));
+                    }
+                    else
+                    {
+                        variables.Add(ParseNormalVariable(vString.Trim(), false));
+                    }
+                }
+                return variables;
+            }
+            else
+            {
+                string[] variableStrings = vString.Split(new[] { "\n" }, StringSplitOptions.None);
+                List<Variable> variables = new List<Variable>();
+                foreach (var varString in variableStrings)
+                {
+                    
+                    if (varString.Trim().StartsWith("+") || varString.Trim().StartsWith("-") || varString.Trim().StartsWith("#"))
+                    {
+                        variables.Add(ParseNormalVariable(varString, false));
+                    }
+
                 }
 
+                return variables;
             }
 
-            return variables;
+
+
+        
+            
         }
+
+        private static Variable ParseNormalVariable(string varString, bool isStatc)
+        {
+            string definedValue = null;
+            string name;
+            if (varString.Contains("="))
+            {
+                definedValue = varString.Split(':')[0].Split('=')[1].Trim();
+                name = varString.Split(':')[0].Split('=')[0].Trim().Remove(0, 1).Trim();
+            }
+            else name = varString.Split(':')[0].Trim().Remove(0, 1).Trim();
+
+            bool isPrivate = varString.StartsWith("-");
+
+            string objectType = varString.Split(':')[1].Trim();
+            bool isFinal = name.ToUpper().Equals(name);
+
+
+            return new Variable { Name = name, IsPrivate = isPrivate, IsStatic = isStatc, IsFinal = isFinal, ObjectType = objectType, DefinedValue = definedValue };
+
+        }
+
     }
 }
